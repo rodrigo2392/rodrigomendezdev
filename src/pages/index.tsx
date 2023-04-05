7752d4ecf5b8085aa94d8fa506f81913d890215e
Rodrigo Mendez <itic.rodrigomg@gmail.com>
2023-04-05 12:06:56 -0600

    chore: seo tags optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import Social from '@/components/Social'
 import {getVideos} from '../contentful'
 import {YoutubeVideo} from '../contentful/types'
 import {Bounce ,JackInTheBox} from 'react-awesome-reveal';
+import Meta from '@/components/Meta';
 
 const poppins = Poppins({
   weight: ['400', '700'],
@@ -49,9 +50,35 @@ export default function Home({videos}: Props) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
         <title>Rodrigo Méndez - Frontend developer</title>
-        <meta name="description" content="Rodrigo Méndez - Frontend developer" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="Rodrigo Méndez - Frontend developer"/>
+        <meta name="description" content="Rodrigo Méndez desarrollador frontend con 10 años de experiencia" />
+        <meta name="keywords" content="frontend,desarrollador,guadalajara,zapopan,jalisco,backend,fullstack,react,native" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Spanish" />
+        <meta name="revisit-after" content="10 days" />
+        <meta name="author" content="Rodrigo Méndez" />
+        <meta itemProp="name" content="Rodrigo Méndez - Frontend developer" />
+        <meta itemProp="description" content="Rodrigo Méndez desarrollador frontend con 10 años de experiencia" />
+
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:title" content="Rodrigo Méndez - Frontend developer" />
+        <meta name="twitter:description" content="Rodrigo Méndez desarrollador frontend con 10 años de experiencia" />
+        <meta name="twitter:site" content="@rodrigom_dev"/>
+        <meta name="twitter:creator" content="@rodrigom_dev"/>
+        <meta name="twitter:image:src" content="https://rodrigomendez.dev/_ipx/w_1080,q_75/%2F_next%2Fstatic%2Fmedia%2Fperfil-c.2d6e09cb.png?url=%2F_next%2Fstatic%2Fmedia%2Fperfil-c.2d6e09cb.png&w=1080&q=75"/>
+        
+        <meta name="og:title" content="Rodrigo Méndez - Frontend developer" />
+        <meta name="og:description" content="Rodrigo Méndez desarrollador frontend con 10 años de experiencia" />
+        <meta name="og:image" content="https://rodrigomendez.dev/_ipx/w_1080,q_75/%2F_next%2Fstatic%2Fmedia%2Fperfil-c.2d6e09cb.png?url=%2F_next%2Fstatic%2Fmedia%2Fperfil-c.2d6e09cb.png&w=1080&q=75" />
+        <meta name="og:url" content="https://rodrigomendez.dev" />
+        <meta name="og:site_name" content="rodrigomendez.dev" />
+        <meta name="og:locale" content="es_MX" />
+        <meta name="og:type" content="website" />
+        
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={poppins.className}>
