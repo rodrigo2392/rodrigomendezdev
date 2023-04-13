0a03e0e5ce94640513dc179b5622a51389c6beea
Rodrigo Mendez <itic.rodrigomg@gmail.com>
2023-04-12 19:43:01 -0600

    fix lint issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     // this will check Typescript files
-    "**/*.(ts|tsx)": () => "npm run build",
+    "Check build": () => "npm run build",
+    "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+
+    // This will lint and format TypeScript and                                             //JavaScript files
+    "**/*.(ts|tsx|js)": filenames => [
+        `yarn eslint --fix ${filenames.join(" ")}`,
+        `yarn prettier --write ${filenames.join(" ")}`,
+    ],
+
+    // this will Format MarkDown and JSON
+    "**/*.(md|json)": filenames =>
+        `yarn prettier --write ${filenames.join(" ")}`,
 };
