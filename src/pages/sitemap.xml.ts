0cb93b94eadf9b399456cee00f660717cd28f4c6
Rodrigo Mendez <itic.rodrigomg@gmail.com>
2023-04-13 00:11:48 -0600

    fix: robots and sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,12 +11,8 @@ interface Props {
 function generateSiteMap({ posts }: Props) {
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
      <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+       <loc>https://rodrigomendez.dev</loc>
      </url>
      ${posts
          .map(({ fields: { slug } }) => {
